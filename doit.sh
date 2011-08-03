7ea08acf306285808d0e0d571a502b275a0b531b
jt1134 <jt1134@gmail.com>
2011-08-03 02:03:59 -0500

    herp script it derp

---
+++ doit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf out
+rm -rf update/data/*.zip
+rm -rf kernel/samsung/aries/build
+
+source build/envsetup.sh
+lunch full_fascinatemtd-eng
+
+./kernel/samsung/aries/build.sh fascinatemtd
+make -j8 bootimage
+
+cp out/target/product/fascinatemtd/boot.img update/data
+cp device/samsung/fascinatemtd/bcm4329.ko update/data/system/lib/modules
+
+cd update/data
+ZIP="KernelUpdate-`date +%m%d`.zip"
+zip -r $ZIP . 
+mv $ZIP ../..
+cd ../..
+
