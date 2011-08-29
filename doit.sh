8edf3e42e2b3bc2a9358183049321a8c9b6e60b6
KhasMek <Boushh@gmail.com>
2011-08-29 16:56:25 -0700

    add README and options to build other mtd kernels

--- doit.sh
+++ doit.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
+DEVICE=${1%}
+
+if [ "$DEVICE" = "" ]; then
+        echo "product is blank, fascinatemtd will be used"
+        DEVICE=fascinatemtd
+else
+        echo "product is $DEVICE"
+fi
+
+echo "rm out"
 rm -rf out
+echo "rm update/data/*.zip"
 rm -rf update/data/*.zip
+echo "rm update/log/*.log"
+rm -rf update/log/*.log
+echo "cleaning build directory"
 rm -rf kernel/samsung/aries/build
 
 source build/envsetup.sh
-lunch full_fascinatemtd-eng
+lunch full_"$DEVICE"-eng
 
-./kernel/samsung/aries/build.sh fascinatemtd
-make -j8 bootimage
+./kernel/samsung/aries/build.sh "$DEVICE"
+make -j$(grep processor /proc/cpuinfo | wc -l) bootimage 1> update/log/stdlog_"$DEVICE".log 2> update/log/errlog_"$DEVICE".log
 
-cp out/target/product/fascinatemtd/boot.img update/data
-cp device/samsung/fascinatemtd/bcm4329.ko update/data/system/lib/modules
+cp out/target/product/"$DEVICE"/boot.img update/data
+cp device/samsung/"$DEVICE"/bcm4329.ko update/data/system/lib/modules
 
 cd update/data
-ZIP="KernelUpdate-`date +%m%d`.zip"
-zip -r $ZIP . 
+ZIP="KernelUpdate-$DEVICE-`date +%m%d`.zip"
+zip -r $ZIP .
 mv -f $ZIP ../..
 cd ../..
 
