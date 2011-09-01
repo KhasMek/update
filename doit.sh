7ed85c737e273015c43b15986acc8bf9f25d2648
KhasMek <Boushh@gmail.com>
2011-09-01 15:52:28 -0700

    fix showcase and mezmerize builds while keeping device naming correct

--- doit.sh
+++ doit.sh
@@ -2,11 +2,15 @@
 
 DEVICE=${1%}
 
-if [ "$DEVICE" = "" ]; then
-        echo "product is blank, fascinatemtd will be used"
-        DEVICE=fascinatemtd
+if [ "$DEVICE" != "vzwtabmtd" ]; then
+        echo "product is blank blank or fascinatemtd compatible, fascinatemtd will be used"
+        CONFIG=fascinatemtd
+	if [ "$DEVICE" = "" ]; then
+		DEVICE=fasinatemtd
+	fi
 else
         echo "product is $DEVICE"
+	CONFIG=$DEVICE
 fi
 
 echo "rm out"
@@ -19,13 +23,13 @@ echo "cleaning build directory"
 rm -rf kernel/samsung/aries/build
 
 source build/envsetup.sh
-lunch full_"$DEVICE"-eng
+lunch full_"$CONFIG"-eng
 
-./kernel/samsung/aries/build.sh "$DEVICE"
+./kernel/samsung/aries/build.sh "$CONFIG"
 make -j$(grep processor /proc/cpuinfo | wc -l) bootimage 1> update/log/stdlog_"$DEVICE".log 2> update/log/errlog_"$DEVICE".log
 
-cp out/target/product/"$DEVICE"/boot.img update/data
-cp device/samsung/"$DEVICE"/bcm4329.ko update/data/system/lib/modules
+cp out/target/product/"$CONFIG"/boot.img update/data
+cp device/samsung/"$CONFIG"/bcm4329.ko update/data/system/lib/modules
 
 cd update/data
 ZIP="KernelUpdate-$DEVICE-`date +%m%d`.zip"
